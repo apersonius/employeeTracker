a59d4ac304c7c4e144076e2c8e47994357ad7c8b
apersonius <personius12@gmail.com>
2023-05-02 18:39:57 -0500

    add role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,11 @@ VALUES
 ('Legal'),
 ('Sales');
 
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 4),
+('Lead Engineer', 150000, 1),
+('Accountant', 125000, 2),
+('Lawyer', 190000, 3);
+
+
