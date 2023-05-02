2b4caf140152446da65de78d905d30f572ea09ce
apersonius <personius12@gmail.com>
2023-05-02 18:17:16 -0500

    add roles table, set pk and fk

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
