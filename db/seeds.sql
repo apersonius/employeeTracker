edad81b29e61db64c7d1bd604f38dceadb186e7b
apersonius <personius12@gmail.com>
2023-05-03 13:06:30 -0500

    add employee seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,4 +12,11 @@ VALUES
 ('Accountant', 125000, 2),
 ('Lawyer', 190000, 3);
 
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('John', 'Doe', 4, NULL),
+('Ashley', 'Rodriguez', 1, NULL),
+('Malia', 'Brown', 2, NULL),
+('Tom', 'Allen', 3, 1),;
+
 
