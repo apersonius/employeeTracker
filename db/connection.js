f896439327b78453c73f6ecae9951623ef5789fa
apersonius <personius12@gmail.com>
2023-05-03 13:28:16 -0500

    create config file, hide password in .env

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,10 @@
-// config file
+const mysql = require('mysql2');
+
+const connect = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: process.env.SQLPASSWORD,
+    databade: 'company_db'
+});
+
+module.exports = connect;
